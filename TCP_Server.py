acc8f39fc94feda9536ba6f5981503968d7d3676
huang <huang@magedu.com>
2018-11-06 17:07:32 +0800

    TCP的服务端改用select机制监控IO操作

--- TCP_Server.py
+++ TCP_Server.py
@@ -1,15 +1,25 @@
+import selectors
 import socket
 import threading
 import logging
 
 
 class GroupChatServer:
-    def __init__(self, ip: str, port: int):
+    def __init__(self, ip, port):
         self.address = ip, port
+        self.server_socket = socket.socket()
+        self.selector = selectors.DefaultSelector()
         self.group = {}
         self.event = threading.Event()
         self.log = self.getlogger()
-        self.lock_group = threading.Lock()
+        self.setup()
+
+    def start(self):
+        """
+        Create anther thread,
+        which is use to supervise the network IO.
+        """
+        threading.Thread(target=self.select, name='select').start()
 
     @staticmethod
     def getlogger():
@@ -29,86 +39,98 @@ class GroupChatServer:
         log.propagate = False
         return log
 
-    def start(self):
-        """Create anther thread,
-        which is use to accept connection from client.
-        The main thread will not block.
+    def setup(self):
         """
-        threading.Thread(target=self.terminate, name='terminate').start()
-        threading.Thread(target=self.accept, name='accept', daemon=True).start()
+        Do some pre-work.
+        """
+        self.server_socket.bind(self.address)
+        self.server_socket.listen(32)  # .listen函数的backlog, 指定半连接的最大数量, 一般不超过30
+        self.server_socket.setblocking(False)
+        socket_key = self.selector.register(self.server_socket, selectors.EVENT_READ, data=self.accept)
+        self.fd = socket_key.fd
 
     def accept(self):
         """
         Use to accept connections of clients
-        until GroupChatServer object calls terminate function.
+        and register the new socket to selector.
         """
-        with socket.socket() as s:
-            s.bind(self.address)
-            s.listen()
-            while True:  # 不停地循环, 反正有accept阻塞
-                client, c_address = s.accept()
-                self.lock_group.acquire()
-                self.group[c_address] = client  # 这个key是一个二元tuple
-                self.lock_group.release()
+        client, address = self.server_socket.accept()
+        self.group[address] = client
+        client.setblocking(False)  # "推荐改成非阻塞模式", 但是为什么???
+        self.log.info('Client {}:{} join the group chat.'.format(address[0], address[1]))
+        self.selector.register(client, selectors.EVENT_READ, data=self.handle)
 
-                self.log.info('Client {}:{} join the group chat.'.format(c_address[0], c_address[1]))
-                threading.Thread(target=self.receive,
-                                 args=(client, c_address),
-                                 name='{}:{}'.format(c_address[0], c_address[1]),
-                                 daemon=True).start()
+    def handle(self, sock: selectors.SelectorKey):  # Server端不需要读写分离, 收到信息, 转发出去就好
+        """
+        Receive client's message.
+        and send the message to other group members.
+        """
+        try:
+            data, address = sock.fileobj.recvfrom(1024)
+            # 这个socket对象, 是Server端和Client端连接的socket对象, 不是Client端的socket对象,
+            # 所以这个socket的local address是Server的address, remote address是Client的address.
+            if data == b'quit':
+                self.quit(address)
+            else:
+                print(data)
+                for a, c in self.group.items():
+                    if a == address:
+                        continue
+                    c.send(data)
+        except:
+            self.quit(sock.fileobj.getpeername())
 
-    def receive(self, client: socket.socket, caddr: tuple):
+    def select(self):
         """
-        Create a thread is ready to receive client's message.
-        each client has his own receive thread (start in accept function).
+        Create a thread to supervise network IO.
         """
-        with client:
-            while True:
-                try:
-                    data = client.recv(1024)  # 在这阻塞, 等待接收client的消息
-                    if data == b'quit':  # or data == b'':  # 这里修改一下, 感觉是软件的问题, 主动断连接一直发送b"
-                        self.quit(caddr)
-                        break
-                    message = '{}: {}'.format(caddr[1], data.decode(encoding='utf8'))
-                    # print('{}'.format(message))  # 测试用的, 服务端不需要显示信息
-                    for a, c in self.group.items():
-                        if a == caddr:
-                            continue
-                        c.send(message.encode(encoding='utf8'))
-                except Exception:
-                    self.quit(caddr)
-                    break
+        while not self.event.is_set():
+            events = self.selector.select()  # 这里面有个timeout参数
+            for sk, mask in events:
+                if sk.fd == self.fd:
+                    sk.data()
+                else:
+                   sk.data(sk)
 
-    def quit(self, caddr):
+    def quit(self, address):
         """
-        When client sends 'quit' or terminates the connection,
-        this function will be called,
-        in order kick the client out from the group and notice another clients.
+        Kick client out who send the message "quit" or terminates connection.
         """
-        message = 'Client {}:{} is quit.'.format(caddr[0], caddr[1])
+        message = 'Client {}:{} is quit.'.format(address[0], address[1])
         self.log.info(message)
+        client = self.group.pop(address)
+        self.selector.unregister(client)
+        client.close()
+        for a, c in self.group.items():
+            if a == address:
+                continue
+            c.send(message.encode(encoding='utf8'))
 
-        self.lock_group.acquire()
-        self.group.pop(caddr)
-        self.lock_group.release()
-
-        if len(self.group) == 0:
-            self.event.set()
-        else:
-            for a, c in self.group.items():
-                if a == caddr:
-                    continue
-                c.send(message.encode(encoding='utf8'))
-
-    def terminate(self):
-        """Terminate this group chat program when members is ."""
-        self.event.wait()
+    def finish(self):
+        """
+        Stop the server.
+        """
+        self.event.set()
+        self.selector.close()
+        for c in self.group.values():
+            self.selector.unregister(c)
+            c.close()
+        self.server_socket.close()
         self.log.info('Group chat is finish.')
 
 
 def main():
-    s = GroupChatServer('127.0.0.1', 9999)
+    ip = '127.0.0.1'
+    port = 9999
+    s = GroupChatServer(ip, port)
     s.start()
+    while True:
+        cmd = input('Server command : ')
+        if cmd == 'Server stop':
+            s.finish()
+
+            break
+        print(s.group)
 
 
 if __name__ == '__main__':
